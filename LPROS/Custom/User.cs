8c2e70362049159c8e2e8d4aecf24183c02194e4
furkan <furkan@DESKTOP-2NLM8C0>
2019-04-20 14:39:37 +0300

    siparisadd

---
+++ LPROS/Custom/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPROS.Custom
+{
+    class User
+    {
+        static public int id { get; set; }
+    }
+}
